d27e3686f339ba3be2ff68f9aba841ddb95ce1df
Adrian Hesketh <adrianhesketh@hushmail.com>
2021-07-17 11:15:27 +0100

    strip DynamoDB type information from outbound events

--- handler/handler.go
+++ handler/handler.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"reflect"
+	"strconv"
 	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -87,7 +89,10 @@ func createOutboundEvent(r map[string]events.DynamoDBAttributeValue) (id, eventT
 	for i := 0; i < len(keysToDelete); i++ {
 		delete(r, keysToDelete[i])
 	}
-	detailJSON, err := json.Marshal(r)
+	// Strip type data.
+	m := stripDynamoDBTypesFromMap(r)
+	// Get JSON.
+	detailJSON, err := json.Marshal(m)
 	if err != nil {
 		return
 	}
@@ -100,6 +105,64 @@ func createOutboundEvent(r map[string]events.DynamoDBAttributeValue) (id, eventT
 		Detail:       &detail,
 	}
 	return
+}
+
+func stripDynamoDBTypesFromMap(m map[string]events.DynamoDBAttributeValue) map[string]interface{} {
+	op := map[string]interface{}{}
+	for k := range m {
+		k := k
+		op[k] = getAttributeValue(m[k])
+	}
+	return op
+}
+
+func stripDynamoDBTypesFromList(list []events.DynamoDBAttributeValue) []interface{} {
+	op := make([]interface{}, len(list))
+	for i := 0; i < len(list); i++ {
+		op[i] = getAttributeValue(list[i])
+	}
+	return op
+}
+
+func getAttributeValue(av events.DynamoDBAttributeValue) interface{} {
+	switch av.DataType() {
+	case events.DataTypeBinary:
+		return av.Binary()
+	case events.DataTypeBoolean:
+		return av.Boolean()
+	case events.DataTypeBinarySet:
+		return av.BinarySet()
+	case events.DataTypeList:
+		return stripDynamoDBTypesFromList(av.List())
+	case events.DataTypeMap:
+		return stripDynamoDBTypesFromMap(av.Map())
+	case events.DataTypeNumber:
+		return getNumber(av.Number())
+	case events.DataTypeNumberSet:
+		return av.NumberSet()
+	case events.DataTypeNull:
+		return nil
+	case events.DataTypeString:
+		return av.String()
+	case events.DataTypeStringSet:
+		return av.StringSet()
+	default:
+		panic(fmt.Sprintf("unknown DynamoDBAttributeValue type: %v", reflect.TypeOf(av.DataType())))
+	}
+}
+
+func getNumber(s string) interface{} {
+	// First try integer.
+	i, err := strconv.ParseInt(s, 10, 64)
+	if err == nil {
+		return i
+	}
+	// Then float.
+	f, err := strconv.ParseFloat(s, 64)
+	if err == nil {
+		return f
+	}
+	panic(fmt.Sprintf("cannot get number from %q", s))
 }
 
 func batch(values []*eventbridge.PutEventsRequestEntry, n int) (pages [][]*eventbridge.PutEventsRequestEntry) {
---
+++ handler/handler_test.go
@@ -0,0 +1,80 @@
+package handler
+
+import (
+	"testing"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestStripDynamoDBTypes(t *testing.T) {
+	var tests = []struct {
+		name     string
+		input    map[string]events.DynamoDBAttributeValue
+		expected map[string]interface{}
+	}{
+		{
+			name: "",
+			input: map[string]events.DynamoDBAttributeValue{
+				"Binary":  events.NewBinaryAttribute([]byte{0xDE, 0xAD, 0xBE, 0xEF}),
+				"Boolean": events.NewBooleanAttribute(true),
+				"BinarySet": events.NewBinarySetAttribute([][]byte{
+					{0xDE, 0xAD, 0xBE, 0xEF},
+					{0x0D, 0x15, 0xEA, 0x5E},
+				}),
+				"List": events.NewListAttribute([]events.DynamoDBAttributeValue{
+					events.NewStringAttribute("a"),
+					events.NewNumberAttribute("1"),
+				}),
+				"Map": events.NewMapAttribute(map[string]events.DynamoDBAttributeValue{
+					"innerA": events.NewStringAttribute("value"),
+					"innerB": events.NewMapAttribute(map[string]events.DynamoDBAttributeValue{
+						"innerInner": events.NewStringAttribute("innerInnerValue"),
+					}),
+				}),
+				"Number (int, -)":   events.NewNumberAttribute("-1"),
+				"Number (int, +)":   events.NewNumberAttribute("2000"),
+				"Number (float, -)": events.NewNumberAttribute("-0.3"),
+				"Number (float)":    events.NewNumberAttribute("0.0"),
+				"Number (float, +)": events.NewNumberAttribute("+0.3"),
+				"NumberSet":         events.NewNumberSetAttribute([]string{"0", "0.5", "1"}),
+				"Null":              events.NewNullAttribute(),
+				"String":            events.NewStringAttribute("string value"),
+				"StringSet":         events.NewStringSetAttribute([]string{"A", "B"}),
+			},
+			expected: map[string]interface{}{
+				"Binary":  []byte{0xDE, 0xAD, 0xBE, 0xEF},
+				"Boolean": true,
+				"BinarySet": [][]byte{
+					{0xDE, 0xAD, 0xBE, 0xEF},
+					{0x0D, 0x15, 0xEA, 0x5E},
+				},
+				"List": []interface{}{"a", int64(1)},
+				"Map": map[string]interface{}{
+					"innerA": "value",
+					"innerB": map[string]interface{}{
+						"innerInner": "innerInnerValue",
+					},
+				},
+				"Number (int, -)":   int64(-1),
+				"Number (int, +)":   int64(2000),
+				"Number (float, -)": -0.3,
+				"Number (float)":    0.0,
+				"Number (float, +)": 0.3,
+				"NumberSet":         []string{"0", "0.5", "1"},
+				"Null":              nil,
+				"String":            "string value",
+				"StringSet":         []string{"A", "B"},
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			actual := stripDynamoDBTypesFromMap(tt.input)
+			if diff := cmp.Diff(tt.expected, actual); diff != "" {
+				t.Error(diff)
+			}
+		})
+	}
+}
