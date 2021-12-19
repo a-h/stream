942d01a24ec623d7578f77fac4e0d9359b53500d
Daniel J. Rollins <daniel@djrollins.com>
2021-12-19 14:31:04 +0000

    fix(store): fix duplicate sort keys after multiple input events are processed
    BREAKING CHANGE: This changes the storage representation of events in
    DynamoDB and is not backwards compatible.

--- integration_test.go
+++ integration_test.go
@@ -61,70 +61,6 @@ type Sum int
 func (ai Sum) EventName() string { return "Sum" }
 func (ai Sum) IsOutbound()       {}
 
-// BUG: Multiples of the same INBOUND event type breaks future writes
-// because the sort key is not namespaced by STATE's sequence number,
-// but uses this number as a base to start incrementing from.
-//
-// It also increments _seq for each event. This does not cause writes
-// to fail but it does mean that the INBOUND/OUTBOUND events are not
-// associated with the state transition in which they were
-// consumed/produced.
-//
-// Consider this code:
-//	state := &AverageState{}
-//      p, _ := stream.New(store, "<id>", state)
-// 	_ = p.Process(Add{10}, Add{15}, Add{20})
-//
-// This creates items with the following sort keys and sequence numbers:
-// 	| no:  | _pk:         | _sk:          | _seq:  | new/updated: |
-//	|------|--------------|---------------|--------|--------------|
-//	|    1 | Average/<id> | STATE         |      1 | *            |
-//	|    2 | Average/<id> | INBOUND/Add/1 |      1 | *            |
-//	|    3 | Average/<id> | INBOUND/Add/2 |      2 | *            |
-//	|    4 | Average/<id> | INBOUND/Add/3 |      3 | *            |
-//
-// A future write occurs e.g.:
-//	state := &AverageState{}
-//      p, _ := stream.Load(store, "<id>", state)
-// 	_ = p.Process(Add{10})
-//
-// This attempts to update the table in this way
-// 	| no:  | _pk:         | _sk:          | _seq:  | new/updated: |
-//	|------|--------------|---------------|--------|--------------|
-//	|    1 | Average/<id> | STATE         |      2 | *            |
-//	|    2 | Average/<id> | INBOUND/Add/1 |      1 |              |
-//	|    3 | Average/<id> | INBOUND/Add/2 |      2 |              |
-//	|    4 | Average/<id> | INBOUND/Add/3 |      3 |              |
-//	|    5 | Average/<id> | INBOUND/Add/2 |      2 | *            |
-//
-// Items 5 ends up clashing with item 2 which fails the condition check and the
-// write transaction fails.
-//
-// IMO, the event sort keys should be of the form:
-// 	`<EventType>/<STATE-_seq>/<EventIndex>/<EventName>`
-//
-// Resulting in the following behaviour
-//
-// First write:
-// 	| no:  | _pk:         | _sk:            | _seq:  | new/updated: |
-//	|------|--------------|-----------------|--------|--------------|
-//	|    1 | Average/<id> | STATE           |      1 | *            |
-//	|    2 | Average/<id> | INBOUND/1/1/Add |      1 | *            |
-//	|    3 | Average/<id> | INBOUND/1/2/Add |      1 | *            |
-//	|    4 | Average/<id> | INBOUND/1/3/Add |      1 | *            |
-//
-// Second write:
-// 	| no:  | _pk:         | _sk:            | _seq:  | new/updated: |
-//	|------|--------------|-----------------|--------|--------------|
-//	|    1 | Average/<id> | STATE           |      2 | *            |
-//	|    2 | Average/<id> | INBOUND/1/1/Add |      1 |              |
-//	|    3 | Average/<id> | INBOUND/1/2/Add |      1 |              |
-//	|    4 | Average/<id> | INBOUND/1/3/Add |      1 |              |
-//	|    5 | Average/<id> | INBOUND/2/1/Add |      2 | *            |
-//
-// This does make _seq for the events redundant, as the sort key maintains the
-// ordering itself, but I don't see any harm in that. We should probably update
-// the OUTBOUND events to be in the same format as well, for consistency.
 func TestIntegration(t *testing.T) {
 	if testing.Short() {
 		t.Skip()
@@ -151,17 +87,25 @@ func TestIntegration(t *testing.T) {
 		t.Fatalf("first processing run failed: %v", err)
 	}
 
+	err = p.Process(Add{10})
+	if err == nil {
+		t.Fatal("second processing without reload should have failed, but didn't")
+	}
+
 	// Reload and process again
 	p, err = Load(store, id, state)
 	if err != nil {
 		t.Fatalf("failed to load record with id %v: %v", id, err)
 	}
 
-	// BUG: This can never succeed due the aforementioned bug
+	dumpTable(store)
+
 	err = p.Process(Add{10})
 	if err != nil {
 		t.Fatalf("second processing run failed: %v", err)
 	}
+
+	dumpTable(store)
 }
 
 func dumpTable(store *DynamoDBStore) {
